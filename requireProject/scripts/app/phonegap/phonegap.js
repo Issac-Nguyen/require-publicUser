486cc7244195267892fbf2c3792e31d18daa38b3
Chong Lun Chan <chancl@surbana.com>
2015-06-22 10:48:08 +0000

    Changes made by Chong Lun Chan (chancl@surbana.com).

--- requireProject/scripts/app/phonegap/phonegap.js
+++ requireProject/scripts/app/phonegap/phonegap.js
@@ -4,6 +4,7 @@ define([], function() {
         createContact: function(Testcontact){
              var myContact = navigator.contacts.create(Testcontact);
             myContact.note = "This contact has a note.";
+            myContact.save(function(){}, function(err){alert(err);});
             console.log("The contact, " + myContact.displayName + ", note: " + myContact.note);
         }
     }
